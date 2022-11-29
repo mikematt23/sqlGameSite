228e18f26af1e68c1359110bdc561a5837ec90cb
Michael MAtteis <michaelcmatteis@gmail.com>
2022-11-28 23:59:49 -0600

    adding the session store and setting up a few session varibles isAuth and error message

--- app.js
+++ app.js
@@ -1,16 +1,39 @@
 const express = require('express')
-const { appendFile } = require('fs')
+const session = require('express-session')
+const mysql2 = require('mysql2/promise')
+const MySQlStore = require('express-mysql-session')(session)
 const path = require('path')
 const siteRoutes = require('./routes/siteGetRoutes')
 const postRoutes = require('./routes/sitePostRoutes')
 
+
 const app = express()
+
+const options = {
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: "@SSassin55",
+  database: 'game_site'
+}
+
+const connection = mysql2.createPool(options)
+const sessionStore = new MySQlStore({},connection)
+
+app.use(session({
+  secret : "mine",
+  cookie: {maxAge:30000},
+  saveUninitialized : false,
+  resave: false,
+  store: sessionStore
+}))
 
 app.set('view engine',"ejs")
 app.set("views", path.join(__dirname,'views'))
 
 app.use(express.urlencoded({extended:true}))
 app.use(express.static('public'))
+
 app.use(siteRoutes)
 app.use(postRoutes)
 
--- data/database.js
+++ data/database.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql2/promise')
 
 const pool = mysql.createPool({
+  connectionLimit : 10, 
   connectionLimit:20,
-  host: 'localhost',
- 
+
 })
 
 

9073aa0ef3eca672584b56aa45c9d105bac4a43e
Michael MAtteis <michaelcmatteis@gmail.com>
2023-01-12 13:14:53 -0600

    seeing if this works

--- data/database.js
+++ data/database.js
@@ -1,7 +1,11 @@
 const mysql = require('mysql2/promise')
 
 const pool = mysql.createPool({
-
+  connectionLimit:20,
+  host: '127.0.0.1',
+  user:"root",
+  password:"@SSassin55",
+  database : "game_site"
 })
 
 
